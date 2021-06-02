d60a34a13287527ea766fc9a6d6b6d44e4e9f220
CoasterFreak <coasterfreak@melion.net>
2021-06-02 17:04:27 +0200

    Fixed Console Colors

--- src/main/kotlin/cloud/melion/extensions/ORKBaseExtensions.kt
+++ src/main/kotlin/cloud/melion/extensions/ORKBaseExtensions.kt
@@ -1,4 +1,4 @@
 /* (C)2021 */
 package cloud.melion.extensions
 
-fun String.send() = println("\u001B[42mORK\u001B[37m » $this")
+fun String.send() = println("\u001B[32mORK\u001B[37m » $this\u001B[0m")
