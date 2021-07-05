9d033f104f9313631d4dc6dcaf494334280682c1
TheFruxz <cedricspitzer@outlook.de>
2021-07-05 12:54:56 +0200

    No explizit specify required at only-return functions

--- src/main/kotlin/cloud/melion/base/GetConstructorFunction.kt
+++ src/main/kotlin/cloud/melion/base/GetConstructorFunction.kt
@@ -1,10 +1,8 @@
 /* (C)2021 */
 package cloud.melion.base
 
-import java.lang.reflect.Constructor
-
-fun getConstructor(table: Class<*>, primaryKeys: Array<out String>): Constructor<*>? {
-	return table.constructors.find { constructor ->
+fun getConstructor(table: Class<*>, primaryKeys: Array<out String>) =
+	table.constructors.find { constructor ->
 		primaryKeys.all {
 			it in
 					constructor.parameterAnnotations.map { parameter ->
@@ -12,4 +10,3 @@ fun getConstructor(table: Class<*>, primaryKeys: Array<out String>): Constructor
 					}
 		}
 	}
-}
