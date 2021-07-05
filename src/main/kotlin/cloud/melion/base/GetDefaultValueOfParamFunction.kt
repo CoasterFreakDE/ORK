79f8eee4c0dba32973c7ed148cf3870de40b81e0
TheFruxz <cedricspitzer@outlook.de>
2021-07-05 12:59:05 +0200

    Returns can be merged into single-return-function, so it can be instant

--- src/main/kotlin/cloud/melion/base/GetDefaultValueOfParamFunction.kt
+++ src/main/kotlin/cloud/melion/base/GetDefaultValueOfParamFunction.kt
@@ -3,13 +3,13 @@ package cloud.melion.base
 
 import cloud.melion.annotations.FieldName
 
-fun getDefaultValueOfParam(fieldParam: Any): String {
+fun getDefaultValueOfParam(fieldParam: Any) =
 	if (fieldParam is FieldName) {
 		if (fieldParam.defaultValue != "NULL") {
-			return fieldParam.defaultValue
+			fieldParam.defaultValue
 		} else {
 			throw NullPointerException("DefaultValue is null")
 		}
-	}
-	throw NullPointerException("FieldParam is null")
-}
+	} else
+		throw NullPointerException("FieldParam is null")
+
