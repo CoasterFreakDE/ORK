92c987f5abdbc90adbad4634c6b9c07f4c62abdd
TheFruxz <cedricspitzer@outlook.de>
2021-07-05 12:25:27 +0200

    Shorter define

--- src/main/kotlin/cloud/melion/base/QueryFunction.kt
+++ src/main/kotlin/cloud/melion/base/QueryFunction.kt
@@ -4,6 +4,4 @@ package cloud.melion.base
 import cloud.melion.MySQL
 import java.sql.ResultSet
 
-fun query(sql: String, result: (ResultSet) -> Unit) {
-  MySQL.onQuery(sql, result)
-}
+fun query(sql: String, result: (ResultSet) -> Unit) = MySQL.onQuery(sql, result)
